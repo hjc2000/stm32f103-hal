351325d96fbf0c8f967a6bc6710fbf2d5bdd0e58
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-07 00:42:38 +0800

    USE_HAL_SRAM_REGISTER_CALLBACKS

--- include/stm32f1xx_hal_conf.h
+++ include/stm32f1xx_hal_conf.h
@@ -149,7 +149,7 @@
 #define  USE_HAL_SD_REGISTER_CALLBACKS          0U /* SD register callback disabled        */
 #define  USE_HAL_SMARTCARD_REGISTER_CALLBACKS   0U /* SMARTCARD register callback disabled */
 #define  USE_HAL_IRDA_REGISTER_CALLBACKS        0U /* IRDA register callback disabled      */
-#define  USE_HAL_SRAM_REGISTER_CALLBACKS        1U /* SRAM register callback disabled      */
+#define  USE_HAL_SRAM_REGISTER_CALLBACKS        0U /* SRAM register callback disabled      */
 #define  USE_HAL_SPI_REGISTER_CALLBACKS         0U /* SPI register callback disabled       */
 #define  USE_HAL_TIM_REGISTER_CALLBACKS         1U /* TIM register callback disabled       */
 #define  USE_HAL_UART_REGISTER_CALLBACKS        1U /* UART register callback disabled      */
